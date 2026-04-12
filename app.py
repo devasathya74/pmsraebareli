a276cde467018ced3ee52aaa5cf5ab1279d2ae25
Sathya Deva <sathyauppolice74@gmail.com>
2026-04-12 19:21:13 +0530

    Refactor app initialization and add home route

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-import os 
-from school_app import create_app
+from flask import Flask
+import os
 
+app = Flask(__name__)
 
-app = create_app()
-
+@app.route("/")
+def home():
+    return "App is working"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
